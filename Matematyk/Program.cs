0ce9b5a1727e599c141cdd9dbd9dfb7e4aa2fe1f
MrHatchling <bibooj@gmail.com>
2024-02-10 12:29:23 +0100

    Dodaj pliki projektów.

---
+++ Matematyk/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Matematyk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            double num = rnd.Next(0, 16);
+            double num2 = rnd.Next(0, 10);            
+            double correct = 0;
+
+            switch (rnd.Next(0, 4)) 
+            {
+                case 0:
+                    Console.Write($"{num} + {num2} = ");
+                    correct = num + num2;
+                    break;
+                case 1:
+                    Console.Write($"{num} - {num2} = ");
+                    correct = num - num2;
+                    break;
+                case 2:
+                    Console.Write($"{num} * {num2} = ");
+                    correct = num * num2;
+                    break;
+                case 3:
+                    num2 = rnd.Next(1, 3);
+                    Console.Write($"{num} / {num2} = ");
+                    correct = num / num2;
+                    break;
+            }
+
+            string res = Console.ReadLine();
+            double result = Convert.ToDouble(res);
+
+            if (result == correct) 
+            {
+                Console.WriteLine("You are right, my man!");
+            }
+            else
+            {
+                Console.WriteLine("You are wrong, you idiot!");
+            }
+        }
+    }
+}
